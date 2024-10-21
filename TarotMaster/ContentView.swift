f79b8f820f99505b26873b1051fcdd48a48b62e6
stevenyu253436 <stevenyu.supreme@gmail.com>
2024-10-21 11:50:43 +0800

    Initial Commit

---
+++ TarotMaster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TarotMaster
+//
+//  Created by 游哲維 on 2024/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TarotMaster/TarotMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarotMasterApp.swift
+//  TarotMaster
+//
+//  Created by 游哲維 on 2024/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TarotMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
